29736c40ea982cf6d76d57396e90448fa17baf2c
Mac <maclong9@icloud.com>
2025-02-25 17:19:00 +0000

    feat: add loop capabilities and tests

--- Tests/WebUITests/ElementTests.swift
+++ Tests/WebUITests/ElementTests.swift
@@ -5,6 +5,7 @@ import Testing
 // MARK: - Element Tests
 @Suite("Element Tests")
 struct ElementTests {
+  let numbers = ["one", "two", "three"]
   let html = Article {
     Text(heading: .h1) { "Hello, world!" }
     Section(id: "main-content") {
@@ -97,5 +98,46 @@ struct ElementTests {
   @Test("Render text with id and classes")
   func testTextWithAttributesRendering() async throws {
     #expect(html.contains("<span id=\"text-id\" class=\"text-class\">Styled text.</span>"))
+  }
+}
+
+// MARK: - Flow Control Tests
+@Suite("Flow Control Tests") struct FlowControlTests {
+  @Test("Renders conditionally")
+  func rendersConditionally() async throws {
+    let isVisible = true
+    let html = Section {
+      if isVisible {
+        Text { "Content Visible" }
+      }
+    }.render()
+
+    #expect(html.contains("<section><span>Content Visible</span></section>"))
+  }
+
+  @Test("Renders Conditionally with Else")
+  func rendersConditionallyWithElse() async throws {
+    let isVisible = false
+    let html = Section {
+      if isVisible {
+        Text { "Content Visible" }
+      } else {
+        Text { "Content Not Visible" }
+      }
+    }.render()
+    
+    #expect(html.contains("<section><span>Content Not Visible</span></section>"))
+  }
+
+  @Test("Renders Loop")
+  func rendersLoop() async throws {
+    let numbers: [Int] = [1, 2, 3, 4, 5]
+    let html = Section {
+      for number in numbers {
+        Text { "\(number)" }
+      }
+    }.render()
+    
+    #expect(html.contains("<section><span>1</span><span>2</span><span>3</span><span>4</span><span>5</span></section>"))
   }
 }
