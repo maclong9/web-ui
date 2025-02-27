0b91c28fec23aab91c91421c84abd1475c2c2de3
Mac <maclong9@icloud.com>
2025-02-27 13:16:32 +0000

    refactor: improve process for overriding values on a document basis

--- Sources/WebUI/Core/Document.swift
+++ Sources/WebUI/Core/Document.swift
@@ -1,12 +1,15 @@
 import Foundation
 
-// TODO: Create bundles of css and js based on page title and only load the css and js needed
+// TODO: Create optimised bundles of css and js named `{page-title}.css`
 
 /// Represents a document containing content, configuration, and rendering logic.
 public struct Document {
   let configuration: Configuration
   let title: String
-  let description: String?
+  let description: String
+  let keywords: [String]?
+  let author: String?
+  let type: String?
   let headerOverride: HeaderVariant?
   let footerOverride: FooterVariant?
 
@@ -14,7 +17,7 @@ public struct Document {
 
   /// The computed property that evaluates the content builder to get the nested HTML components.
   var content: [any HTML] {
-    get { contentBuilder() }
+    contentBuilder()
   }
 
   /// Creates a new `Document` instance.
@@ -22,14 +25,18 @@ public struct Document {
   /// - Parameters:
   ///   - configuration: The configuration settings.
   ///   - title: The document title.
-  ///   - description: An optional description.
+  ///   - description: A description of the document content.
+  ///   - keywords: An array of keywords for seo.
   ///   - content: The main content of the document.
   ///   - headerOverride: An optional header override.
   ///   - footerOverride: An optional footer override.
   init(
     configuration: Configuration = Configuration(),
     title: String,
-    description: String?,
+    description: String,
+    keywords: [String]? = nil,
+    author: String? = nil,
+    type: String? = nil,
     headerOverride: HeaderVariant? = nil,
     footerOverride: FooterVariant? = nil,
     @HTMLBuilder content: @escaping () -> [any HTML]
@@ -37,6 +44,9 @@ public struct Document {
     self.configuration = configuration
     self.title = title
     self.description = description
+    self.keywords = keywords
+    self.author = author
+    self.type = type
     self.headerOverride = headerOverride
     self.footerOverride = footerOverride
     self.contentBuilder = content
@@ -48,6 +58,12 @@ public struct Document {
   func render() -> String {
     let pageTitle = "\(title) | \(configuration.metadata.site)"
     let renderedContent = content.map { $0.render() }.joined()
+    let twitterSocial = URL(
+      string: configuration.socials?.first {
+        $0.label.lowercased().contains("twitter") || $0.label.lowercased().contains("x")
+          || $0.url.contains("twitter.com")
+          || $0.url.contains("x.com")
+      }?.url ?? "")?.lastPathComponent
 
     // Create a LayoutRenderer with the appropriate variants
     let layoutRenderer = LayoutRenderer(
@@ -56,28 +72,21 @@ public struct Document {
       footerVariant: footerOverride ?? configuration.layout.footer
     )
 
-    let metadata = configuration.metadata
-
     return """
       <!DOCTYPE html>
       <html lang="\(configuration.metadata.locale)">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          \(metadata.author.map { "<meta name=\"author\" content=\"\($0)\">" } ?? "")
-          \(description.map { "<meta name=\"description\" content=\"\($0)\">" } ?? "")
-          \(metadata.keywords.map { "<meta name=\"keywords\" content=\"\($0.joined(separator: ", "))\">" } ?? "")
-          \(metadata.twitter.map { "<meta name=\"twitter:creator\" content=\"\($0)\">" } ?? "")
-          \(metadata.type.map { "<meta property=\"og:type\" content=\"\($0)\">" } ?? "")
-          <meta property="og:title" content="\(pageTitle)">
-          \(description.map { "<meta property=\"og:description\" content=\"\($0)\">" } ?? "")
-          <title>\(pageTitle)</title>
-          <link rel="stylesheet" href="/\(title.replacingOccurrences(of: " ", with: "-")).css">
-        </head>
+        \(configuration.metadata.render(
+          pageTitle: pageTitle,
+          description: description,
+          twitter: twitterSocial,
+          author: author,
+          keywords: keywords,
+          type: type
+        ))
         <body>
-          \(layoutRenderer.renderHeader())
+          \(layoutRenderer.renderHeader() ?? "")
           \(renderedContent)
-          \(layoutRenderer.renderFooter())
+          \(layoutRenderer.renderFooter() ?? "")
         </body>
       </html>
       """
