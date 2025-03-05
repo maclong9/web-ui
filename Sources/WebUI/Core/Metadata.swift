332065351894d199c4f52011202761317774a477
Mac <maclong9@icloud.com>
2025-03-05 17:48:56 +0000

    fix: add space to seperator for title rendering

--- Sources/WebUI/Core/Metadata.swift
+++ Sources/WebUI/Core/Metadata.swift
@@ -90,7 +90,7 @@ public struct Metadata {
         \(effectiveKeywords?.isEmpty == false
               ? "<meta name=\"keywords\" content=\"\(effectiveKeywords?.joined(separator: ", ") ?? "")\">"
               : "")
-        <link rel="stylesheet" href="/\(pageTitle.split(separator: "|")[0].replacingOccurrences(of: " ", with: "-").lowercased()).css">
+        <link rel="stylesheet" href="/\(pageTitle.split(separator: " |")[0].replacingOccurrences(of: " ", with: "-").lowercased()).css">
         <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
       </head>
       """
