7276b3b7b2e4b9692e61b455911b8fca3cbc4ab9
Mac Long <hello@maclong.uk>
2025-03-17 16:52:04 +0100

    test: add Form element tests

--- Tests/WebUITests/Elements/FormTests.swift
+++ Tests/WebUITests/Elements/FormTests.swift
@@ -5,16 +5,39 @@ import Testing
 @Suite("Form Element Tests") struct FormTests {
   @Test("The form element should render")
   func formElementShouldRender() throws {
-    
+    let html = Form(action: "/api/login") {
+      Stack {
+        Label(for: "email") { "Email Address" }
+        Input(name: "email", type: .email, placeholder: "mac@mail.com")
+      }
+      Stack {
+        Label(for: "password") { "Password" }
+        Input(name: "password", type: .password, placeholder: "********")
+      }
+      Button(type: .submit) { "Login" }
+    }.render()
+
+    #expect(
+      html == """
+        <form action="/api/login" method="post"><div><label for="email">Email Address</label><input type="email" placeholder="mac@mail.com"></div><div><label for="password">Password</label><input type="password" placeholder="********"></div><button type="submit">Login</button></form>
+        """)
   }
-  
+
   @Test("The form element should render with get method")
   func renderWithGetMethod() throws {
-    
+    let html = Form(action: "/api/update", method: .get) {
+      Button(type: .submit) { "Update" }
+    }.render()
+
+    #expect(
+      html == """
+        <form action="/api/update" method="get"><button type="submit">Update</button></form>
+        """)
   }
   
-  @Test("The form element should render with a custom action")
-  func renderWithCustomAction() throws {
-    
+  @Test("Should render a progess indicator correctly")
+  func progressIndicatorRendersCorrectly() throws {
+    let html = Progress(value: 65, max: 100).render()
+    #expect(html == #"<progress value="65.0" max="100.0">"#)
   }
 }
