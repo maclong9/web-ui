440d8987a2fb66ddf273402f9ef1ebdfbb24461d
Mac Long <hello@maclong.uk>
2025-04-17 12:06:11 +0100

    chore: make package name lowercase and hyphenated

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-  name: "WebUI",
+  name: "web-ui",
   platforms: [.macOS(.v13)],
   products: [
     .library(name: "WebUI", targets: ["WebUI"])
