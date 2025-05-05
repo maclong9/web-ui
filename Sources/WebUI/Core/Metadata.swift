2a8ee7df4879d18736cdef7e0c927b192bef92fe
Mac <maclong9@icloud.com>
2025-05-05 17:02:39 +0100

    fix: add convenience initialiser for extending metadata from a previous object

--- Sources/WebUI/Core/Metadata.swift
+++ Sources/WebUI/Core/Metadata.swift
@@ -73,7 +73,8 @@ public struct Metadata {
     self.site = site
     self.title = title
     self.titleSeperator = titleSeperator
-    self.pageTitle = "\(title ?? "")\(site.map { " \(titleSeperator) \($0)" } ?? "")"
+    self.pageTitle =
+      "\(title ?? "")\(site.map { " \(titleSeperator) \($0)" } ?? "")"
     self.description = description
     self.date = date
     self.image = image
@@ -83,5 +84,54 @@ public struct Metadata {
     self.locale = locale
     self.type = type
     self.themeColor = themeColor
+  }
+}
+
+extension Metadata {
+  /// Initializes a new `Metadata` instance based on an existing one, with optional modifications to any property.
+  ///
+  /// - Parameters:
+  ///   - base: The original `Metadata` instance.
+  ///   - site: Optional site value, default is `nil`.
+  ///   - title: Optional title value, default is `nil`.
+  ///   - titleSeperator: Optional title separator, default is the same as `base`.
+  ///   - description: Optional description value, default is the same as `base`.
+  ///   - date: Optional date value, default is the same as `base`.
+  ///   - image: Optional image value, default is the same as `base`.
+  ///   - author: Optional author value, default is the same as `base`.
+  ///   - keywords: Optional keywords value, default is the same as `base`.
+  ///   - twitter: Optional twitter handle, default is the same as `base`.
+  ///   - locale: Optional locale value, default is the same as `base`.
+  ///   - type: Optional content type, default is the same as `base`.
+  ///   - themeColor: Optional theme color, default is the same as `base`.
+  public init(
+    base: Metadata,
+    site: String? = nil,
+    title: String? = nil,
+    titleSeperator: String? = nil,
+    description: String? = nil,
+    date: Date? = nil,
+    image: String? = nil,
+    author: String? = nil,
+    keywords: [String]? = nil,
+    twitter: String? = nil,
+    locale: Locale? = nil,
+    type: ContentType? = nil,
+    themeColor: ThemeColor? = nil
+  ) {
+    self.site = site ?? base.site
+    self.title = title ?? base.title
+    self.titleSeperator = titleSeperator ?? base.titleSeperator
+    self.pageTitle =
+      "\(title ?? "")\(site.map { " \(titleSeperator ?? "|") \($0)" } ?? "")"
+    self.description = description ?? base.description
+    self.date = date ?? base.date
+    self.image = image ?? base.image
+    self.author = author ?? base.author
+    self.keywords = keywords ?? base.keywords
+    self.twitter = twitter ?? base.twitter
+    self.locale = locale ?? base.locale
+    self.type = type ?? base.type
+    self.themeColor = themeColor ?? base.themeColor
   }
 }
