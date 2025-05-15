a6e2278e697e49b9997558b892921c3322896217
Mac <maclong9@icloud.com>
2025-05-15 20:23:29 +0100

    docs: correct paths etc. for docs

---
+++ Sources/WebUI/Documentation/WebUI.docc/Resources/code-files/terminal-sources.sh
@@ -0,0 +1,5 @@
+mkdir -p Sources/MyStaticSite/Components
+touch Sources/Pages/Components/Layout.swift
+touch Sources/Pages/Home.swift
+touch Sources/Pages/About.swift
+touch Sources/Application.swift
