0b607d113cb809d76e668cb2c429873e17c86712
Mac <maclong9@icloud.com>
2025-05-21 20:16:36 +0100

    style: formatting

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "WebUIMarkdown", targets: ["WebUIMarkdown"]),
     ],
     dependencies: [
-                .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-markdown", from: "0.6.0"),
         .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.4.3"),
     ],
